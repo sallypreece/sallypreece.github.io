107feae90749b20a4f88d3bb27444cc28bef47e9
Andy Bower <bower@bowerhaus.eu>
2016-01-31 22:05:11 +0000

    Template .py file

--- python.py
+++ python.py
@@ -1,6 +1,29 @@
-from browser import document, alert
 
-def echo(event):
-    alert(document["zone"].value)
+from browser import document
+from io import StringIO
 
-document['mybutton'].bind('click', echo)
+def showQuestions():
+	out = StringIO()
+	out.write('<b>Questions</b>')
+	out.write('<p>')
+	out.write('These are the questions')
+	questions = document['questions']
+	questions.html=out.getvalue()
+	out.close()
+
+def showAnswers():
+	out = StringIO()
+	out.write('<b>Answers</b>')
+	out.write('<p>')
+	out.write('These are the answers')
+	answers = document['answers']
+	answers.html = out.getvalue()
+	out.close()
+
+def revealAnswers():
+	showAnswers()
+
+showQuestions()
+
+revealButton=document['revealId']
+revealButton.bind("click", revealAnswers)
